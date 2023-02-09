25e2f44672dba1e117751c1ef79ed9aa431c66db
Vuong Nguyen <trungvuong2169@gmail.com>
2023-02-09 16:11:49 +0700

    #53

--- src/containers/Header/Header.js
+++ src/containers/Header/Header.js
@@ -5,22 +5,47 @@ import * as actions from "../../store/actions";
 import Navigator from "../../components/Navigator";
 import { adminMenu } from "./menuApp";
 import "./Header.scss";
-
+import { LANGUAGES } from "../../utils";
+// import { changeLanguageApp } from "../../store/actions";
 class Header extends Component {
+  changLanguage = (language) => {
+    this.props.changeLanguageAppRedux(language);
+  };
   render() {
     const { processLogout } = this.props;
-
+    let language = this.props.language;
     return (
       <div className="header-container">
         {/* thanh navigator */}
         <div className="header-tabs-container">
           <Navigator menus={adminMenu} />
         </div>
-
-        {/* nút logout */}
-        <div className="btn btn-logout" onClick={processLogout}>
-          <i className="fas fa-sign-out-alt"></i>
+        <div className="languages">
+          <span
+            className={
+              language === LANGUAGES.VI ? "language-vi active" : "language-vi"
+            }
+            onClick={() => this.changLanguage(LANGUAGES.VI)}
+          >
+            VN
+          </span>
+          <span
+            className={
+              language === LANGUAGES.EN ? "language-en active" : "language-en"
+            }
+            onClick={() => this.changLanguage(LANGUAGES.EN)}
+          >
+            EN
+          </span>
+          <div
+            className="btn btn-logout"
+            onClick={processLogout}
+            title="Log out"
+          >
+            <i className="fas fa-sign-out-alt"></i>
+          </div>
         </div>
+        {/* nút logout */}
       </div>
     );
   }
@@ -29,12 +54,15 @@ class Header extends Component {
 const mapStateToProps = (state) => {
   return {
     isLoggedIn: state.user.isLoggedIn,
+    language: state.app.language,
   };
 };
 
 const mapDispatchToProps = (dispatch) => {
   return {
     processLogout: () => dispatch(actions.processLogout()),
+    changeLanguageAppRedux: (languageInput) =>
+      dispatch(actions.changeLanguageApp(languageInput)),
   };
 };
 
