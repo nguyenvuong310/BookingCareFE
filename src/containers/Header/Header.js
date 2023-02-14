0eb3d5d463a64d2231c557cf006bee230eba7204
Vuong Nguyen <trungvuong2169@gmail.com>
2023-02-14 22:21:12 +0700

    #55

--- src/containers/Header/Header.js
+++ src/containers/Header/Header.js
@@ -6,14 +6,16 @@ import Navigator from "../../components/Navigator";
 import { adminMenu } from "./menuApp";
 import "./Header.scss";
 import { LANGUAGES } from "../../utils";
+import { FormattedMessage } from "react-intl";
 // import { changeLanguageApp } from "../../store/actions";
 class Header extends Component {
   changLanguage = (language) => {
     this.props.changeLanguageAppRedux(language);
   };
   render() {
-    const { processLogout } = this.props;
+    const { processLogout, userInfo } = this.props; // let processLogout = this.props.processLogout
     let language = this.props.language;
+    console.log(userInfo);
     return (
       <div className="header-container">
         {/* thanh navigator */}
@@ -21,6 +23,10 @@ class Header extends Component {
           <Navigator menus={adminMenu} />
         </div>
         <div className="languages">
+          <span className="welcome">
+            <FormattedMessage id="home-header.welcome" />,{" "}
+            {userInfo && userInfo.user.lastName ? userInfo.user.lastName : ""}
+          </span>
           <span
             className={
               language === LANGUAGES.VI ? "language-vi active" : "language-vi"
@@ -54,6 +60,7 @@ class Header extends Component {
 const mapStateToProps = (state) => {
   return {
     isLoggedIn: state.user.isLoggedIn,
+    userInfo: state.user.userInfo,
     language: state.app.language,
   };
 };
--- src/containers/Header/menuApp.js
+++ src/containers/Header/menuApp.js
@@ -1,22 +1,64 @@
 export const adminMenu = [
   {
-    //hệ thống
-    name: "menu.system.header",
+    //quan ly nguoi dung
+    name: "menu.admin.manage",
+    menus: [
+      // subMenus: [
+      //   {
+      //     name: "menu.system.system-administrator.user-manage",
+      //     link: "/system/user-manage",
+      //   },
+      //   {
+      //     name: "menu.system.system-administrator.product-manage",
+      //     link: "/system/user-redux",
+      //   },
+      // ],
+      {
+        name: "menu.admin.crud",
+        link: "/system/user-manage",
+      },
+      {
+        name: "menu.admin.crud-redux",
+        link: "/system/user-redux",
+      },
+      {
+        name: "menu.admin.manage-doctor",
+        link: "/system/user-manage-doctor",
+      },
+      {
+        name: "menu.admin.manage-admin",
+        link: "/system/user-manage-admin",
+      },
+    ],
+  },
+  {
+    //quan ly phong kham
+    name: "menu.admin.clinic",
     menus: [
       {
-        name: "menu.system.system-administrator.header",
-        subMenus: [
-          {
-            name: "menu.system.system-administrator.user-manage",
-            link: "/system/user-manage",
-          },
-          {
-            name: "menu.system.system-administrator.product-manage",
-            link: "/system/user-redux",
-          },
-        ],
+        name: "menu.admin.manage-clinic",
+        link: "/system/user-manage",
       },
-      // { name: 'menu.system.system-parameter.header', link: '/system/system-parameter' },
+    ],
+  },
+  {
+    //quan ly specialty
+    name: "menu.admin.specialty",
+    menus: [
+      {
+        name: "menu.admin.manage-specialty",
+        link: "/system/manage-specialty",
+      },
+    ],
+  },
+  {
+    //quan ly cam nang
+    name: "menu.admin.handbook",
+    menus: [
+      {
+        name: "menu.admin.manage-handbook",
+        link: "/system/manage-handbook",
+      },
     ],
   },
 ];
